5ba4f3417b6e69384d99199d6e35227fed325084
Jeffrei S. Moreira <jeffreism@yahoo.com>
2024-05-06 17:30:08 -0400

    First commit

---
+++ src/main/java/com/moreira/jeffrei/GoToCardGame/GoToCardGameApplication.java
@@ -0,0 +1,13 @@
+package com.moreira.jeffrei.GoToCardGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoToCardGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoToCardGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moreira/jeffrei/GoToCardGame/GoToCardGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moreira.jeffrei.GoToCardGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoToCardGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
